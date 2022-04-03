8f55d882824d2d2fdfe98f14fedfea31c8f7e446
Victor <vitya898989@gmail.com>
2022-04-03 18:56:44 +0300

    project_card

---
+++ src/js/script.js
@@ -0,0 +1,57 @@
+let slider0304;
+
+if (window.innerWidth <= 959) {
+    slider0304 = new Swiper('.slider_0304', {
+        slidesPerView: 'auto',
+        spaceBetween: 25,
+    });
+} else {
+    slider0304 = new Swiper('.slider_0304', {
+        navigation: {
+            nextEl: '.slider_0304__btn_next',
+            prevEl: '.slider_0304__btn_prev'
+        },
+        spaceBetween: 25,
+        slidesPerView: 1,
+        grid: {
+            rows: 3,
+        },
+    });
+}
+
+let review0304ImgWr = document.querySelector('.review_0304__img_wr');
+let review0304Info = document.querySelector('.review_0304__info');
+let review0304Author = document.querySelector('.review_0304__author');
+
+if (window.innerWidth <= 639) {
+    review0304ImgWr.append(review0304Author);
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth <= 639) {
+        review0304ImgWr.append(review0304Author);
+    } else {
+        review0304Info.append(review0304Author);
+    }
+    if (window.innerWidth <= 959) {
+        slider0304.destroy();
+        slider0304 = new Swiper('.slider_0304', {
+            slidesPerView: 'auto',
+            spaceBetween: 25,
+        });
+    } else {
+        slider0304.destroy();
+        slider0304 = new Swiper('.slider_0304', {
+            navigation: {
+                nextEl: '.slider_0304__btn_next',
+                prevEl: '.slider_0304__btn_prev'
+            },
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grid: {
+                rows: 3,
+            },
+        });
+    }
+});
+
